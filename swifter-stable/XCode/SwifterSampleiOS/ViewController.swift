9e205374a3bdde06a7a161a02aa72c4209d0736f
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2018-03-05 11:54:38 -0600

    swifter library

---
+++ swifter-stable/XCode/SwifterSampleiOS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Swifter
+//  Copyright (c) 2015 Damian Kołakowski. All rights reserved.
+//
+
+import UIKit
+import Swifter
+
+class ViewController: UIViewController {
+    
+    private var server: HttpServer?
+    
+    @IBOutlet weak var webView2: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do {
+            let server = demoServer(Bundle.main.resourcePath!)
+            try server.start(9080)
+            self.server = server
+        } catch {
+            print("Server start error: \(error)")
+        }
+    }
+    
+    @IBAction func likedThis(_ sender: UIButton) {
+        self.server?.stop()
+        self.server = nil
+    }
+}
